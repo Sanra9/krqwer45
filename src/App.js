157ad5b62a3391a495284b4741cf0ea7d91556f3
sanra9 <sanrra999@gmail.com>
2019-06-07 11:34:24 -0500

    jé2

--- src/App.js
+++ src/App.js
@@ -3,62 +3,66 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
+    this.updateTask = this.updateTask.bind(this)
+    this.addTask = this.addTask.bind(this)
+    this.updateDone = this.updateDone.bind(this)
     this.state = {
       tasks: [
         { id: 1, name: "Sacar la ropa", done: false },
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      isBlank: false
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="list">
+          <h3>Por hacer:</h3>
+          <ul className="todo">
+            {this.state.tasks.map((task, index) =>
+              <li key={task.id} className={task.done == true ? 'done': null} onClick={() => this.updateDone(task.id)}>{task.name}</li>
+            )}
+          </ul>
+          <form onSubmit={this.addTask}>
+            <input type="text" id="new-task" className={this.state.isBlank ? 'error' : null} onChange={this.updateTask} placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          </form>
+        </div>
+      </div>
+    )
   }
 
+  addTask(event){
+    event.preventDefault();
+    if (this.state.newTask == '')
+      this.setState({
+        isBlank: true
+      });
+    else
+      this.setState({
+        tasks: this.state.tasks.concat({id: this.state.tasks.length + 1, name: this.state.newTask, done: false}),
+        newTask: '',
+        isBlank: false
+      });
+  }
 
-  handleChange(event){
+  updateTask(event){
     this.setState({
       newTask: event.target.value
     })
   }
 
-  handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      this.setState({
-        tasks: this.state.tasks.concat({id:this.state.tasks.length + 1, name:this.state.newTask, done:false}),
-        newTask: '',
-      });
-      event.preventDefault();
-    }
-  }
-
-  updateTasksDone(taskId){
+  updateDone(taskId){
     this.setState({
       tasks: this.state.tasks.map((task) => {
         if(task.id === taskId) task.done = !task.done;
         return task;
       })
     })
-  }
-  
-  render() {
-    console.log(this.state.tasks)
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            {this.state.tasks.map((task, index) => 
-            <li key={task.id} className={task.done == true ? 'done': null} onClick={() => this.updateTasksDone(task.id)}>{task.name}</li>)}
-          </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" name="name" value={this.state.newTask} onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
-          </form>
-        </div>
-      </div>
-    )
   }
 }
 
